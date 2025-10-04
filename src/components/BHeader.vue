195ec48e8be1c5e656b2aed774a867f8a8af7b98
Callum Docherty <callum.docherty1@monash.edu>
2025-10-05 00:42:17 +1000

    added firestore GCP function, GetBookCount

--- src/components/BHeader.vue
+++ src/components/BHeader.vue
@@ -35,6 +35,11 @@
         <li class="nav-item">
           <router-link to="/logout" class="nav-link" active-class="active">Log out</router-link>
         </li>
+        <li class="nav-item">
+          <router-link to="/GetBookCount" class="nav-link" active-class="active"
+            >Get Book Count</router-link
+          >
+        </li>
       </ul>
     </header>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import FirebaseSigninView from '@/views/FirebaseSigninView.vue'
 import FirebaseRegisterView from '@/views/FirebaseRegisterView.vue'
 import AddBookView from '@/views/AddBookView.vue'
 import LogoutView from '@/views/LogoutView.vue'
+import GetBookCountView from '@/views/GetBookCountView.vue'
 
 const routes = [
   {
@@ -28,6 +29,7 @@ const routes = [
   { path: '/FireRegister', name: 'FireRegister', component: FirebaseRegisterView },
   { path: '/addbook', name: 'Addbook', component: AddBookView },
   { path: '/logout', name: 'Logout', component: LogoutView, meta: { requiresAuth: true } },
+  { path: '/GetBookCount', name: 'GetBookCount', component: GetBookCountView },
 ]
 
 const router = createRouter({
