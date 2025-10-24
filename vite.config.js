f1cb43488442c17a5fb0d89bbfcc7602238b55b1
Callum Docherty <callum.docherty1@monash.edu>
2025-10-24 23:39:37 +1100

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/FIT5032-eFolios/',          // 👈 add this line
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
